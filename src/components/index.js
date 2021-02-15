7fa8cf12e1ec4fd1cace0698a16eb6d565e8497e
ereyes <eliel@offshore-team.io>
2021-02-15 21:39:43 +0800

    update export

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,9 @@
 import OffshoreButton from './Button';
 import OffshoreTextField from './TextField'
+import OffshoreSignIn from './SignIn'
 
 export { 
     OffshoreButton,
-    OffshoreTextField
+    OffshoreTextField,
+    OffshoreSignIn
 };
